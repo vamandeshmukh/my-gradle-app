c3d496562912c827d05f5a3beb44d105f87c5f8b
vamandeshmukh <vamandeshmukh@gmail.com>
2021-03-23 11:09:39 +0530

    Commit
    Commit

--- src/test/java/my/gradle/app/CalcTests.java
+++ src/test/java/my/gradle/app/CalcTests.java
@@ -2,6 +2,11 @@ package my.gradle.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 //TDD - Test driven development 
@@ -19,23 +24,45 @@ import org.junit.jupiter.api.Test;
 //4. Refactor the code, if the test fails. 
 //5. Run the test again to pass. 
 
-
-
 public class CalcTests {
+
+	private static Calc calc;
+
+	@BeforeAll
+	public static void setUp() {
+		calc = new Calc();
+		System.out.println("BeforeAll");
+	}
+
+	@BeforeEach
+	public void someMethod() {
+		System.out.println("BeforeEach");
+	}
+
+	@AfterAll
+	public static void tearDown() { // like finally block
+		calc = null;
+		System.out.println("AfterAll");
+	}
+
+	@AfterEach
+	public void someOtherMethod() {
+		System.out.println("AfterEach");
+	}
 
 	@Test
 	public void testAdd() {
-		Calc calc = new Calc();
-		int expected = 10;
+		int expected = 10; // 11
 		int actual = calc.add(5, 5);
-		assertEquals(expected, actual);
+		assertEquals(expected, actual, "Wrong addition");
+		System.out.println("testAdd");
 	}
 
 	@Test
 	public void testNotAdd() {
-		Calc calc = new Calc();
 		int unexpected = 11;
 		int actual = calc.add(5, 5);
 		assertNotEquals(unexpected, actual);
+		System.out.println("testNotAdd");
 	}
 }
